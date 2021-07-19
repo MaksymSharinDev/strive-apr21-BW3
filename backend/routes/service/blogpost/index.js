ea5c48a59c17063207efb003f20652aa150bebda
Mike-OxHuge <mikelitoris34@icloud.com>
2021-07-19 11:57:23 +0200

    finishes basic crud

---
+++ backend/routes/service/blogpost/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import PostModel from "./schema.js";
+const router = express.Router();
+
+router
+  .route("/")
+  .get(async (req, res, next) => {
+    try {
+      const posts = await PostModel.find();
+      res.status(200).send(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const newPost = new PostModel(req.body);
+      await newPost.save();
+      res.status(201).send(newPost);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+router
+  .route("/:id")
+  .get(async (req, res, next) => {
+    try {
+      const post = await PostModel.findById(req.params.id);
+      res.status(200).send(post);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .put(async (req, res, next) => {
+    try {
+      const post = await PostModel.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.status(200).send(`Post with ID ${post._id} has been updated!`);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      await PostModel.findByIdAndDelete(req.params.id);
+      res.status(200).send("post has been deleted!");
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+export default router;
--- backend/routes/service/index.js
+++ backend/routes/service/index.js
@@ -1,21 +1,13 @@
-import express from 'express'
+import express from "express";
 const router = express.Router();
+import blogPostsRouter from "./blogpost/index.js";
 
-router.post('/', (req, res) => {
+router.use("/blogposts", blogPostsRouter);
 
-})
-router.get('/', (req, res) => {
+router.post("/", (req, res) => {});
+router.get("/", (req, res) => {});
+router.get("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {});
 
-})
-router.get('/:id', (req, res) => {
-
-})
-router.put('/:id', (req, res) => {
-
-})
-router.delete('/:id', (req, res) => {
-
-})
-
-export default router
-
+export default router;
