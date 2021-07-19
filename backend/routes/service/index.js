5b2faed4f2e05da9714cabc592c88caa6f531f27
Mike-OxHuge <mikelitoris34@icloud.com>
2021-07-19 12:24:49 +0200

    removes subroutes

--- backend/routes/service/index.js
+++ backend/routes/service/index.js
@@ -4,10 +4,10 @@ import blogPostsRouter from "./blogpost/index.js";
 
 router.use("/blogposts", blogPostsRouter);
 
-router.post("/", (req, res) => {});
-router.get("/", (req, res) => {});
-router.get("/:id", (req, res) => {});
-router.put("/:id", (req, res) => {});
-router.delete("/:id", (req, res) => {});
+// router.post("/", (req, res) => {});
+// router.get("/", (req, res) => {});
+// router.get("/:id", (req, res) => {});
+// router.put("/:id", (req, res) => {});
+// router.delete("/:id", (req, res) => {});
 
 export default router;
