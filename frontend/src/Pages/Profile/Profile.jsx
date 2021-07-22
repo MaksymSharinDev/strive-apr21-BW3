d4fd33833371185bc51d11b95979699f302856eb
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-07-23 01:49:52 +0200

    fix: port 80

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -3,10 +3,22 @@ import Activities from "./Formation/FormationSection/Activities";
 import Skills from "./Formation/FormationSection/Skills";
 import About from "./Formation/About";
 import ExperienceContainer from "./Experience/ExperienceContainer";
+import {Button} from "react-bootstrap";
 
+const PDFButton = () => {
+    let _id = '60f59083c787c18bbe53e3e9'
+    return (
+        <Button
+
+            href={`${process.env.REACT_APP_BACKEND_URL}/api/v1/profile/${_id}/CV`}>
+            download CV
+        </Button>
+    )
+}
 
 const Profile = () => (
     <>
+        <PDFButton/>        
         <About/>
         <Activities/>
         <ExperienceContainer/>
